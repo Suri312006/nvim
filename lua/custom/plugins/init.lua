2d3e0d3bba35460bb6e276244159bafe590f520d
suri <suri312006@gmail.con>
2024-06-10 22:26:13 -0700

    added markdown.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -101,6 +101,14 @@ return {
     config = true, -- or `opts = {}`
   },
 
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('render-markdown').setup({})
+    end,
+  },
   -- language
   'othree/html5.vim',
   'pangloss/vim-javascript',

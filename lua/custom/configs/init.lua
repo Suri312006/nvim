65ecaebc27f4432c67789ccfa45d6671e6fd4a98
suri <suri312006@gmail.con>
2024-06-13 16:08:14 -0700

    capabilities?

--- lua/custom/configs/init.lua
+++ lua/custom/configs/init.lua
@@ -9,5 +9,8 @@ require("custom.configs.vimtex")
 require("custom.configs.themes")
 
 -- require("custom.configs.nordic")
-vim.cmd([[ hi SignColumn guibg=Normal]]) -- removes the shading from the side column
-require('lspconfig').clangd.setup {}     -- lsp setup for clang?
+vim.cmd([[ hi SignColumn guibg=Normal]]) -- removes the shading from the side column ( major w )
+-- require('lspconfig').clangd.setup {}     -- lsp setup for clang? holy moly it worked
+local lspconfig = require("lspconfig")
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+lspconfig.clangd.setup({ capabilities = lsp_capabilities })

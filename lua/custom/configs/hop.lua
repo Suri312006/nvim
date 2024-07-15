be573ddb9dad62d9f706b7a4a4eb483dabdb56b2
suri312006 <suri312006@gmail.com>
2024-07-15 16:05:26 -0700

    big find

--- lua/custom/configs/hop.lua
+++ lua/custom/configs/hop.lua
@@ -1,5 +1,6 @@
 -- place this in one of your configuration file(s)
 local hop = require('hop')
+
 local directions = require('hop.hint').HintDirection
 vim.keymap.set('n', 'f', function()
   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
@@ -7,9 +8,15 @@ end, { remap = true })
 vim.keymap.set('n', 'F', function()
   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
 end, { remap = true })
-vim.keymap.set('n', 't', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+
+vim.keymap.set('n', '<C-f>', function()
+  hop.hint_char1({ current_line_only = false, multi_windows = true })
 end, { remap = true })
-vim.keymap.set('n', 'T', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-end, { remap = true })
+
+
+-- vim.keymap.set('n', 't', function()
+--   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+-- end, { remap = true })
+-- vim.keymap.set('n', 'T', function()
+--   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+-- end, { remap = true })

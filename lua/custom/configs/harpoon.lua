bef864b5d6c145b3a620e16e784f2f1fa3f50085
suri312006 <suri312006@gmail.com>
2024-07-19 16:07:14 -0700

    fixing harpoon

--- lua/custom/configs/harpoon.lua
+++ lua/custom/configs/harpoon.lua
@@ -37,7 +37,7 @@ harpoon:extend({
 -- this part doesnt let you move around or remove snippers
 vim.keymap.set("n", "<C-h>", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end)

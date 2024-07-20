fd7415c0dbe181d8bfa017eb5f03a0735444e0be
suri312006 <suri312006@gmail.com>
2024-07-19 17:39:56 -0700

    trouble so much more useful now

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -79,8 +79,8 @@ return {
       {
 
         "<leader>tt", -- toggle trouble
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
+        "<cmd>Trouble diagnostics toggle <cr>",
+        desc = "Diagnostics (Trouble)",
       },
     }
   },

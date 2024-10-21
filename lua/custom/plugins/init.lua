7640609629f1e125908be483de1c68683acad285
suri312006 <suri312006@gmail.com>
2024-10-20 20:17:56 -0700

    plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,23 +13,23 @@ return {
   {
     'NeogitOrg/neogit',
     dependencies = {
-      'nvim-lua/plenary.nvim',  -- required
+      'nvim-lua/plenary.nvim', -- required
       'sindrets/diffview.nvim', -- optional - Diff integration
 
       -- Only one of these is needed.
       'nvim-telescope/telescope.nvim', -- optional
-      'ibhagwan/fzf-lua',              -- optional
-      'echasnovski/mini.pick',         -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'echasnovski/mini.pick', -- optional
     },
     config = true,
   },
   {
-    "rolv-apneseth/tfm.nvim",
+    'rolv-apneseth/tfm.nvim',
     lazy = false,
     opts = {
       -- TFM to use
       -- Possible choices: "ranger" | "nnn" | "lf" | "vifm" | "yazi" (default)
-      file_manager = "yazi",
+      file_manager = 'yazi',
       -- Replace netrw entirely
       -- Default: false
       replace_netrw = true,
@@ -44,16 +44,16 @@ return {
       -- Custom keybindings only applied within the TFM buffer
       -- Default: {}
       keybindings = {
-        ["<ESC>"] = "q",
+        ['<ESC>'] = 'q',
         -- Override the open mode (i.e. vertical/horizontal split, new tab)
         -- Tip: you can add an extra `<CR>` to the end of these to immediately open the selected file(s) (assuming the TFM uses `enter` to finalise selection)
-        ["<C-v>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.vsplit)<CR>",
-        ["<C-x>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.split)<CR>",
-        ["<C-t>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.tabedit)<CR>",
+        ['<C-v>'] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.vsplit)<CR>",
+        ['<C-x>'] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.split)<CR>",
+        ['<C-t>'] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.tabedit)<CR>",
       },
       -- Customise UI. The below options are the default
       ui = {
-        border = "rounded",
+        border = 'rounded',
         height = 1,
         width = 1,
         x = 0.5,
@@ -64,27 +64,27 @@ return {
       -- Make sure to change these keybindings to your preference,
       -- and remove the ones you won't use
       {
-        "<leader>e",
-        ":Tfm<CR>",
-        desc = "TFM",
+        '<leader>e',
+        ':Tfm<CR>',
+        desc = 'TFM',
       },
       {
-        "<leader>mh",
-        ":TfmSplit<CR>",
-        desc = "TFM - horizontal split",
+        '<leader>mh',
+        ':TfmSplit<CR>',
+        desc = 'TFM - horizontal split',
       },
       {
-        "<leader>mv",
-        ":TfmVsplit<CR>",
-        desc = "TFM - vertical split",
+        '<leader>mv',
+        ':TfmVsplit<CR>',
+        desc = 'TFM - vertical split',
       },
       {
-        "<leader>mt",
-        ":TfmTabedit<CR>",
-        desc = "TFM - new tab",
+        '<leader>mt',
+        ':TfmTabedit<CR>',
+        desc = 'TFM - new tab',
       },
     },
-  }
+  },
   'rhysd/conflict-marker.vim',
   {
     'folke/todo-comments.nvim',
@@ -174,7 +174,7 @@ return {
   {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
-    lazy = false,   -- This plugin is already lazy
+    lazy = false, -- This plugin is already lazy
   },
   {
     'nvim-lualine/lualine.nvim',

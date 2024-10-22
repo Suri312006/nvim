2f6d796bb707a8f9739a62af4656d341ccf0f1cb
suri312006 <suri312006@gmail.com>
2024-10-22 00:49:41 -0700

    more tinkering

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,26 +1,175 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
+-- return {
+--   'nvim-neo-tree/neo-tree.nvim',
+--   version = '*',
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+--     'MunifTanjim/nui.nvim',
+--   },
+--   cmd = 'Neotree',
+--   keys = {
+--     { '<leader>f', ':Neotree reveal right<CR> ', desc = 'NeoTree reveal', silent = true },
+--   },
+--   opts = {
+--     filesystem = {
+--       window = {
+--         mappings = {
+--           ['<leader>f'] = 'close_window',
+--         },
+--       },
+--       hijack_netrw_behavior = 'open_default',
+--     },
+--   },
+-- }
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
+  branch = 'v3.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    {
+      's1n7ax/nvim-window-picker',
+      version = '2.*',
+      config = function()
+        require('window-picker').setup {
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = true,
+            bo = {
+              filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+              buftype = { 'terminal', 'quickfix' },
+            },
+          },
+        }
+      end,
+    },
   },
   cmd = 'Neotree',
   keys = {
     { '<leader>f', ':Neotree reveal right<CR> ', desc = 'NeoTree reveal', silent = true },
   },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['<leader>f'] = 'close_window',
+  config = function()
+    -- Diagnostic icons
+    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+
+    -- Neo-tree setup
+    require('neo-tree').setup {
+      close_if_last_window = false,
+      popup_border_style = 'rounded',
+      enable_git_status = true,
+      enable_diagnostics = true,
+      open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' },
+      sort_case_insensitive = false,
+      default_component_configs = {
+        container = { enable_character_fade = true },
+        indent = {
+          indent_size = 2,
+          padding = 1,
+          with_markers = true,
+          indent_marker = '│',
+          last_indent_marker = '└',
+          highlight = 'NeoTreeIndentMarker',
+          expander_collapsed = '',
+          expander_expanded = '',
+          expander_highlight = 'NeoTreeExpander',
+        },
+        icon = {
+          folder_closed = '',
+          folder_open = '',
+          folder_empty = '󰜌',
+          provider = function(icon, node, state)
+            local success, web_devicons = pcall(require, 'nvim-web-devicons')
+            if success then
+              local devicon, hl = web_devicons.get_icon(node.name)
+              icon.text = devicon or icon.text
+              icon.highlight = hl or icon.highlight
+            end
+          end,
+          default = '*',
+          highlight = 'NeoTreeFileIcon',
+        },
+        modified = { symbol = '[+]', highlight = 'NeoTreeModified' },
+        name = { trailing_slash = false, use_git_status_colors = true, highlight = 'NeoTreeFileName' },
+        git_status = {
+          symbols = {
+            added = '',
+            modified = '',
+            deleted = '✖',
+            renamed = '󰁕',
+            untracked = '',
+            ignored = '',
+            unstaged = '󰄱',
+            staged = '',
+            conflict = '',
+          },
         },
       },
-      hijack_netrw_behavior = 'open_default',
-    },
-  },
+      window = {
+        position = 'right',
+        width = 40,
+        mappings = {
+          ['<leader>f'] = 'close_window',
+          ['<space>'] = { 'toggle_node' },
+          ['<2-LeftMouse>'] = 'open',
+          ['<cr>'] = 'open',
+          ['<esc>'] = 'cancel',
+          ['P'] = { 'toggle_preview', config = { use_float = true } },
+          ['x'] = 'open_split',
+          ['v'] = 'open_vsplit',
+          ['t'] = 'open_tabnew',
+          ['w'] = 'open_with_window_picker',
+          ['C'] = 'close_node',
+          ['z'] = 'close_all_nodes',
+          ['a'] = { 'add', config = { show_path = 'none' } },
+          ['A'] = 'add_directory',
+          ['d'] = 'delete',
+          ['r'] = 'rename',
+          ['y'] = 'copy_to_clipboard',
+          ['p'] = 'paste_from_clipboard',
+          ['c'] = 'copy',
+          ['m'] = 'move',
+          ['q'] = 'close_window',
+          ['R'] = 'refresh',
+          ['?'] = 'show_help',
+          ['<'] = 'prev_source',
+          ['>'] = 'next_source',
+          ['i'] = 'show_file_details',
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = true,
+          hide_gitignored = true,
+          hide_hidden = true,
+        },
+      },
+      buffers = {
+        follow_current_file = { enabled = true },
+        show_unloaded = true,
+      },
+      git_status = {
+        window = {
+          position = 'float',
+          mappings = {
+            ['A'] = 'git_add_all',
+            ['gu'] = 'git_unstage_file',
+            ['ga'] = 'git_add_file',
+            ['gr'] = 'git_revert_file',
+            ['gc'] = 'git_commit',
+            ['gp'] = 'git_push',
+            ['gg'] = 'git_commit_and_push',
+          },
+        },
+      },
+    }
+  end,
 }
